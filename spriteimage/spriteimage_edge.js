1eaee4bdff253ac0d5398f53306bcb8b4bd14845
Satoshi Ohki <roothybrid7@gmail.com>
2012-08-31 15:05:38 +0900

    Implement switch cat pose

--- spriteimage/spriteimage_edge.js
+++ spriteimage/spriteimage_edge.js
@@ -6,6 +6,7 @@
 var im='images/';
 
 var fonts = {};
+   fonts['\'Oregano\', Arial, serif']='<link href=\'http://fonts.googleapis.com/css?family=Oregano\' rel=\'stylesheet\' type=\'text/css\'>';
 
 
 var resources = [
@@ -26,22 +27,251 @@ var symbols = {
             type:'image',
             rect:['0','0','100%','100%','auto','auto'],
             fill:["rgba(0,0,0,0)",im+"bg.jpg",'0%','0%','100%','auto']
+         },
+         {
+            id:'catAnimattion',
+            type:'rect',
+            rect:['25','265','auto','auto','auto','auto']
+         },
+         {
+            id:'RoundRect',
+            type:'rect',
+            rect:['220px','23px','200px','177px','auto','auto'],
+            borderRadius:["10px","10px","10px","10px"],
+            fill:["rgba(255,255,255,1.00)"],
+            stroke:[0,"rgba(0,0,0,1)","none"],
+            boxShadow:["",3,3,3,0,"rgba(0,0,0,0.65)"],
+            c:[
+            {
+               id:'blackcatSprite',
+               type:'rect',
+               rect:['26.2%','auto','auto','auto','auto','-9px']
+            },
+            {
+               id:'Text',
+               type:'text',
+               rect:['49px','0px','auto','auto','auto','auto'],
+               text:"waiting<br>",
+               font:['Oregano, Arial, serif',40,"rgba(34,0,34,1.00)","700","none",""],
+               textShadow:["rgba(0,0,0,0.65)",3,3,3]
+            }]
          }],
          symbolInstances: [
-
+         {
+            id:'blackcatSprite',
+            symbolName:'blackcatSprite'
+         },
+         {
+            id:'catAnimattion',
+            symbolName:'catAnimattion'
+         }
          ]
       },
    states: {
       "Base State": {
+         "${_bg}": [
+            ["style", "height", '100%'],
+            ["style", "background-size", [100,'auto'], {valueTemplate:'@@0@@% @@1@@'} ],
+            ["style", "width", '100%']
+         ],
+         "${_RoundRect}": [
+            ["color", "background-color", 'rgba(255,255,255,1.00)'],
+            ["subproperty", "boxShadow.color", 'rgba(0,0,0,0.65)'],
+            ["style", "left", '220px'],
+            ["style", "width", '200px'],
+            ["style", "top", '23px'],
+            ["style", "height", '176.98334503174px'],
+            ["subproperty", "boxShadow.offsetV", '3px'],
+            ["subproperty", "boxShadow.offsetH", '3px'],
+            ["subproperty", "boxShadow.blur", '3px']
+         ],
+         "${_Text}": [
+            ["subproperty", "textShadow.blur", '3px'],
+            ["subproperty", "textShadow.offsetH", '3px'],
+            ["color", "color", 'rgba(34,0,34,1.00)'],
+            ["style", "font-weight", '700'],
+            ["style", "left", '49px'],
+            ["style", "font-size", '40px'],
+            ["style", "top", '0px'],
+            ["style", "font-family", 'Oregano, Arial, serif'],
+            ["subproperty", "textShadow.color", 'rgba(0,0,0,0.65)'],
+            ["subproperty", "textShadow.offsetV", '3px']
+         ],
          "${_Stage}": [
             ["style", "height", '400px'],
             ["color", "background-color", 'rgba(255,255,255,1)'],
             ["style", "width", '640px']
          ],
-         "${_bg}": [
-            ["style", "height", '100%'],
-            ["style", "background-size", [100,'auto'], {valueTemplate:'@@0@@% @@1@@'} ],
-            ["style", "width", '100%']
+         "${_blackcatSprite}": [
+            ["style", "top", 'auto'],
+            ["style", "left", '20%'],
+            ["style", "bottom", '0px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 8000,
+         autoPlay: true,
+         labels: {
+            "animateEnd": 8000
+         },
+         timeline: [
+         ]
+      }
+   }
+},
+"blackcatSprite": {
+   version: "0.1.7",
+   minimumCompatibleVersion: "0.1.7",
+   build: "0.11.0.164",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   dom: [
+   {
+      rect: ['10px','10px','512px','503px','auto','auto'],
+      transform: {},
+      id: 'cat',
+      type: 'image',
+      clip: ['rect(0px 100px 503px 0px)'],
+      fill: ['rgba(0,0,0,0)','images/blackcat.png','0px','0px']
+   }],
+   symbolInstances: [
+   ]
+   },
+   states: {
+      "Base State": {
+         "${_defenceText}": [
+            ["color", "color", 'rgba(0,0,0,1)'],
+            ["style", "opacity", '1'],
+            ["style", "cursor", 'pointer'],
+            ["style", "font-size", '24px'],
+            ["style", "top", '-23px'],
+            ["transform", "scaleX", '1'],
+            ["style", "width", '103px'],
+            ["style", "font-family", 'Spirax, serif'],
+            ["style", "text-decoration", 'none'],
+            ["style", "left", '41.02px']
+         ],
+         "${_cat}": [
+            ["style", "-webkit-transform-origin", [50,18], {valueTemplate:'@@0@@% @@1@@%'} ],
+            ["style", "-moz-transform-origin", [50,18],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-ms-transform-origin", [50,18],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "msTransformOrigin", [50,18],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-o-transform-origin", [50,18],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["transform", "scaleY", '1'],
+            ["style", "top", '10px'],
+            ["style", "clip", [0,80,90,0], {valueTemplate:'rect(@@0@@px @@1@@px @@2@@px @@3@@px)'} ],
+            ["style", "left", '10px'],
+            ["style", "background-position", [0,0], {valueTemplate:'@@0@@px @@1@@px'} ]
+         ],
+         "${symbolSelector}": [
+            ["style", "height", '110px'],
+            ["style", "width", '120px']
+         ],
+         "${_attackText}": [
+            ["color", "color", 'rgba(0,0,0,1)'],
+            ["style", "opacity", '1'],
+            ["style", "cursor", 'pointer'],
+            ["style", "font-size", '24px'],
+            ["style", "top", '-23.99px'],
+            ["style", "width", '103px'],
+            ["transform", "scaleX", '1'],
+            ["style", "font-family", 'Spirax, serif'],
+            ["style", "text-decoration", 'none'],
+            ["style", "left", '41.02px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 4500,
+         autoPlay: false,
+         labels: {
+            "sitting": 0,
+            "standup": 1000,
+            "running": 2000,
+            "standing": 3000,
+            "waiting": 4000
+         },
+         timeline: [
+            { id: "eid63", tween: [ "transform", "${_cat}", "scaleY", '0.95', { fromValue: '1'}], position: 4000, duration: 250 },
+            { id: "eid64", tween: [ "transform", "${_cat}", "scaleY", '1', { fromValue: '0.95'}], position: 4250, duration: 250 },
+            { id: "eid10", tween: [ "style", "${_cat}", "clip", [0,80,90,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,80,90,0]}], position: 0, duration: 0 },
+            { id: "eid11", tween: [ "style", "${_cat}", "clip", [0,100,90,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,80,503,0]}], position: 1000, duration: 0 },
+            { id: "eid13", tween: [ "style", "${_cat}", "clip", [0,100,90,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,100,90,0]}], position: 2000, duration: 0 },
+            { id: "eid15", tween: [ "style", "${_cat}", "clip", [0,100,90,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,100,503,0]}], position: 3000, duration: 0 },
+            { id: "eid56", tween: [ "style", "${_cat}", "clip", [0,80,90,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,100,90,0]}], position: 4000, duration: 0 },
+            { id: "eid150", tween: [ "style", "${_cat}", "-webkit-transform-origin", [50,18], { valueTemplate: '@@0@@% @@1@@%', fromValue: [50,18]}], position: 4000, duration: 0 },
+            { id: "eid218", tween: [ "style", "${_cat}", "-moz-transform-origin", [50,18], { valueTemplate: '@@0@@% @@1@@%', fromValue: [50,18]}], position: 4000, duration: 0 },
+            { id: "eid219", tween: [ "style", "${_cat}", "-ms-transform-origin", [50,18], { valueTemplate: '@@0@@% @@1@@%', fromValue: [50,18]}], position: 4000, duration: 0 },
+            { id: "eid220", tween: [ "style", "${_cat}", "msTransformOrigin", [50,18], { valueTemplate: '@@0@@% @@1@@%', fromValue: [50,18]}], position: 4000, duration: 0 },
+            { id: "eid221", tween: [ "style", "${_cat}", "-o-transform-origin", [50,18], { valueTemplate: '@@0@@% @@1@@%', fromValue: [50,18]}], position: 4000, duration: 0 },
+            { id: "eid1", tween: [ "style", "${_cat}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [0,0]}], position: 0, duration: 0 },
+            { id: "eid2", tween: [ "style", "${_cat}", "background-position", [-205,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [0,0]}], position: 1000, duration: 0 },
+            { id: "eid12", tween: [ "style", "${_cat}", "background-position", [-90,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [-205,0]}], position: 2000, duration: 0 },
+            { id: "eid14", tween: [ "style", "${_cat}", "background-position", [-300,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [-90,0]}], position: 3000, duration: 0 },
+            { id: "eid57", tween: [ "style", "${_cat}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [-300,0]}], position: 4000, duration: 0 }         ]
+      }
+   }
+},
+"catAnimattion": {
+   version: "0.1.7",
+   minimumCompatibleVersion: "0.1.7",
+   build: "0.11.0.164",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+   dom: [
+   {
+      transform: [{},{},['10deg','0deg']],
+      rect: ['4px','60px','582px','50px','auto','auto'],
+      boxShadow: ['',3,3,3,0,'rgba(0,0,0,0.65)'],
+      id: 'Rectangle',
+      stroke: [0,'rgb(0, 0, 0)','none'],
+      type: 'rect',
+      fill: ['rgba(192,192,192,1)'],
+      c: [
+      {
+         id: 'blackcatSprite',
+         type: 'rect',
+         rect: ['165px','-204px','auto','auto','auto','auto']
+      }]
+   }],
+   symbolInstances: [
+   {
+      id: 'blackcatSprite',
+      symbolName: 'blackcatSprite'
+   }   ]
+   },
+   states: {
+      "Base State": {
+         "${_Rectangle}": [
+            ["style", "top", '60px'],
+            ["subproperty", "boxShadow.offsetH", '3px'],
+            ["subproperty", "boxShadow.color", 'rgba(0,0,0,0.65)'],
+            ["transform", "skewX", '10deg'],
+            ["style", "height", '50px'],
+            ["subproperty", "boxShadow.offsetV", '3px'],
+            ["style", "left", '4.01px'],
+            ["subproperty", "boxShadow.blur", '3px']
+         ],
+         "${_blackcatSprite}": [
+            ["style", "left", '462.25px'],
+            ["style", "top", '-60.35px']
+         ],
+         "${symbolSelector}": [
+            ["style", "height", '513px'],
+            ["style", "width", '1064.0644174738px']
          ]
       }
    },
@@ -50,7 +280,7 @@ var symbols = {
          fromState: "Base State",
          toState: "",
          duration: 0,
-         autoPlay: true,
+         autoPlay: false,
          timeline: [
          ]
       }
--- spriteimage/spriteimage_edgeActions.js
+++ spriteimage/spriteimage_edgeActions.js
@@ -13,7 +13,70 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // よく使用する 
    (function(symbolName) {
       
       
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 0, function(sym, e) {
+         sym.getVariable("switchPose")("sitting");
+      });
+      //Edge binding end
+      
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 2000, function(sym, e) {
+         sym.getVariable("switchPose")("running");
+      });
+      //Edge binding end
+      
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 4000, function(sym, e) {
+         sym.getVariable("switchPose")("standup");
+      });
+      //Edge binding end
+      
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 6000, function(sym, e) {
+         sym.getVariable("switchPose")("standing");
+      });
+      //Edge binding end
+      
+      Symbol.bindTimelineAction(compId, symbolName, "Default Timeline", "complete", function(sym, e) {
+         sym.play(0);
+      });
+      //Edge binding end
+      
+      Symbol.bindSymbolAction(compId, symbolName, "creationComplete", function(sym, e) {
+         /**
+          * Switch cat pose
+          * @param {string} label A edge timeline label.
+          */
+         function switchPose(label) {
+           sym.getSymbol("blackcatSprite").stop(label);
+           sym.$("Text").html(label);
+         }
+         
+         sym.setVariable("switchPose", switchPose);
+      });
+      //Edge binding end
+
    })("stage");
    //Edge symbol end:'stage'
+
+   //=========================================================
+
+   //=========================================================
+   
+   //Edge symbol: 'catAnimation_black'
+   (function(symbolName) {   
+   
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 4500, function(sym, e) {
+         sym.play("waiting");
+
+      });
+      //Edge binding end
+
+   })("blackcatSprite");
+   //Edge symbol end:'blackcatSprite'
+
+   //=========================================================
+   
+   //Edge symbol: 'catAnimattion'
+   (function(symbolName) {   
+   
+   })("catAnimattion");
+   //Edge symbol end:'catAnimattion'
 
 })(jQuery, AdobeEdge, "EDGE-27987461");
