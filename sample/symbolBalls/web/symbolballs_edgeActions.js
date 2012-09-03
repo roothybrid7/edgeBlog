45b22ab0e31ee847c916d05d50ebc2aec1bc68b2
Satoshi Ohki <roothybrid7@gmail.com>
2012-09-03 18:24:49 +0900

    fix bug: setTimeout

--- sample/symbolBalls/web/symbolballs_edgeActions.js
+++ sample/symbolBalls/web/symbolballs_edgeActions.js
@@ -3,14 +3,17 @@
 //Edge symbol: 'stage'
 (function(symbolName){Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","complete",function(sym,e){sym.play(0);});
 //Edge binding end
-Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){sym.getVariable("updateDrop")();});
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){});
 //Edge binding end
 Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){var balls=["redBall","blueBall","greenBall"];function drop(){var ballIndex=Math.floor(Math.random()*9999)%balls.length,ball=sym.createChildSymbol(balls[ballIndex],"RoundRect");ball.play();}
-function updateDrop(){var timeout=Math.random()*sym.getLabelPosition("moveEnd")/4;if(sym.isPlaying()){drop();}
-setTimeout(updateDrop,timeout);}
+function updateDrop(){if(sym.isPlaying()){drop();}}
 sym.setVariable("updateDrop",updateDrop);});
 //Edge binding end
 Symbol.bindElementAction(compId,symbolName,"${_RoundRect}","click",function(sym,e){if(sym.isPlaying()){sym.stop();}else{sym.play();}});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.getVariable("updateDrop")();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3500,function(sym,e){sym.getVariable("updateDrop")();});
 //Edge binding end
 })("stage");
 //Edge symbol end:'stage'
